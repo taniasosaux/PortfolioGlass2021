8927b2c9807d1e1c7853344c0b3455672247d848
Tania Sosa <taniasosaux@gmail.com>
2021-10-02 15:04:11 -0500

    Agregué 3 últimos proyectos
    Tres ultimos proyectos y Pop-up terminado

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ tabsContainer.addEventListener("click", (e)=>{
       aboutSection.querySelector(target).classList.add("active");
   }
 });
+
+/* ---- Portfolio Item Details Popup ---- */
+
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("view-project-btn")){
+        togglePortfolioPopup();
+    }
+})
+
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
